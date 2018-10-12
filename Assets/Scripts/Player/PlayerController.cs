740595f791752e71f95a5cbe9f0033d7cdad1379
MuskettaMan <ev.fdl.ve@gmail.com>
2018-10-12 17:59:12 +0200

    Setup scene

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private float x;
+
+    private Rigidbody2D rb_2d;
+
+    private void Start() {
+        rb_2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update () {
+
+        x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        rb_2d.velocity = new Vector2(x, 0);
+	}
+}
